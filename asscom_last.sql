51da7881408e082db8d1d674cb524179b70e952f
iqbal1606 <ihariosyahputra16@gmail.com>
2021-12-30 14:47:58 +0700

    sql

--- asscom_last.sql
+++ asscom_last.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2021 at 08:41 AM
+-- Generation Time: Dec 30, 2021 at 08:46 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.3.31
 
